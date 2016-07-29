fd32708de4401379df72b74e14fec64b91c26f98
Ordinary citizens <liwsen@sina.com>
2016-07-29 20:59:23 +0800

    blog

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 define(function(require, exports, module) {
 
 	// 通过 require 引入依赖
-	//var $ = require('zepto');
+	var $ = require('zepto');
 
 	// 通过 exports 对外提供接口
 	exports.done = function(){
@@ -11,5 +11,9 @@ define(function(require, exports, module) {
 
 	// 或者通过 module.exports 提供整个接口
 	//module.exports = ...
+
+	exports.header = function(){
+		$('.headerWrapper').height(window.innerHeight);
+	};
 
 });
