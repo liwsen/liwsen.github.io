3138db99777f9b6826bdd1e614742a28789097fd
liwsen <liwsen@sina.com>
2016-12-14 10:49:50 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,99 +1,28 @@
-// 所有模块都通过 define 来定义
-define(function(require, exports, module) {
+define(function(require, exports, module){
+    var $ = require('jquery'); 
 
-	// 通过 require 引入依赖
-	require('zepto');
-	require('zepto-fx');
-	var $ = Zepto;
+    // exports.init = function(options) {
+    //     var DEFAULTS = {};
+    //     this.options = $.extend({}, DEFAULTS, options);
+    // }; 
 
-	// 通过 exports 对外提供接口
-	// exports.done = function(){
-	// 	console.log('test abc...');
-	// };
-	// 或者通过 module.exports 提供整个接口
-	// module.exports = ...
-
-	var w = 600;
-	var h = 750;
-	// 初始化
-	exports.init = function(){
-		//导航栏
-		var $nav = $('#nav');
-		var T;
-		$nav.off().on('mouseover', function(event) {
-			event.preventDefault();
-			clearTimeout(T);
-			$(this).find('.navList').removeClass('navlistOut').addClass('navlistIn').show();
-		})
-		.on('mouseout', function(event) {
-			event.preventDefault();
-			var $list = $(this).find('.navList');
-			$list.removeClass('navlistIn').addClass('navlistOut');
-			T = setTimeout(function(){
-				$list.hide();
-			}, 450);
-		});
-		return this;
-	};
-
-	// width
-	exports.width = function(){
-		var _w = this.height() * w / h;
-		return _w;
-	}
-	// height
-	exports.height = function(){
-		var win_w = window.innerWidth;
-		var win_h = window.innerHeight;
-		if(win_w < win_h && win_w > w){
-			return win_h/2;
-		}else{
-			if(win_w < w){
-				$('.headerWrapper').css({
-					'position': 'relative',
-					'float': 'none'
-				});
-				return win_w * h / w;
-			}
-			return win_h;
-		}
-	}
-	
-	//头部设置
-	exports.header = function(){
-		var my = this;
-		var fn_head = function(){
-			$('.headerWrapper').css({
-				'width': my.width(),
-				'height': my.height()
-			});
-			$('.mainWrapper,.footerWrapper').css({
-				'marginLeft': my.width()
-			});
-		};
-		fn_head();
-		$(window).resize(fn_head);
-		my.headerSide();
-	};
-	
-	//图片切换
-	exports.headerSide = function(){
-		var my = this;
-		var $item = $('.headerSide').find('.item');
-		var i = 0;
-		$item.css({'z-index': 1}).eq(i).css({'z-index': 2});
-		setInterval(function(){
-			if(i < $item.length - 1){
-				i++;
-			}else{
-				i = 0;
-			}
-			$item.eq(i).css({'opacity': 0, 'z-index': 3});
-			$item.eq(i).animate({opacity: 1}, 500, 'ease-in', function(){
-				$(this).css({'z-index': 2});
-				$item.eq(i-1).css({'z-index': 1});
-			});
-		}, 6000);
-	};
+    // 菜单按钮
+    $('#menu_btn').on('click', function(event) {
+    	event.preventDefault();
+    	event.stopPropagation();
+    	var $menu = $('#menu');
+    	if(!$menu.hasClass('show')){
+    		$menu.addClass('show');
+	        $(document).one('click', function(event) {
+	        	$menu.removeClass('show');
+	        });
+    	}else{
+    		$menu.removeClass('show');
+    	}
+    });
+    // 阻止冒泡
+    $('#menu').on('click', function(event) {
+    	event.stopPropagation();
+    });
 
 });
